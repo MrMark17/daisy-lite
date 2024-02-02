9f214432280049cdffebbf27c1aa1bc0899902fa
rizqiNata <rizqiNata@amikom.com>
2024-02-02 09:39:09 +0700

    feat: add style guide

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div class="bg-accent p-5">
+    <h1 class="m-5 p-3 bg-cloud rounded-4xl drop-shadow-regular">Hello world!</h1>
+  </div>
+  <h2>Hello world!</h2>
+  <h3>Hello world!</h3>
+  <h4>Hello world!</h4>
+  <p class="text-body">Hello world!</p>
+  <p class="text-small">Hello world!</p>
+  <p class="text-pre-title">Hello world!</p>
+  <p class="text-btn">Hello world!</p>
+  <a>Hello world!</a>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#ED4B9E",
+      secondary: "#4B4DED",
+      tertiary: "#F3D9DA",
+      dark: "#262641",
+      success: "#46D5B2",
+      text: "#5C5C77",
+      subtleText: "#9797AA",
+      accent: "#EEF2F5",
+      cloud: "#FAFCFE",
+    },
+    fontFamily: {
+      sans: ["Work Sans", "sans-serif"],
+    },
+    extend: {
+      borderRadius: {
+        "4xl": "1.875rem",
+      },
+      dropShadow: {
+        regular: "0 16px 40px rgba(207, 154, 170, .25)",
+      },
+    },
   },
   plugins: [],
 };
